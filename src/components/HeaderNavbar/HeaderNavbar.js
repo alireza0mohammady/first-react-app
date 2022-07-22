ecd064e7e97fa946f0a05e727a3c0c6221af58b7
alireza0mohammady <mr.afg00@gmail.com>
2022-07-22 23:22:40 +0430

    add header component

---
+++ src/components/HeaderNavbar/HeaderNavbar.js
@@ -0,0 +1,8 @@
+
+const HeaderNavbar = () => {
+   return ( <div className="py-3">
+      <h1 className="text-center p-10 text-3xl font-bold text-slate-900">shopping app</h1>
+   </div> );
+}
+ 
+export default HeaderNavbar;
