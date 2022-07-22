7e95ca38e0a3020abd184ab4ae39b2b4e12d8b42
alireza0mohammady <mr.afg00@gmail.com>
2022-07-22 23:39:30 +0430

    replace increment and decrement text to icon , improve ui/ux of decrement (when there is 1 product show trash icon instead of -)

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import { BiMinus } from "react-icons/bi";
+import { BiPlus } from "react-icons/bi";
+import { BiTrash } from "react-icons/bi";
 const Product = (props) => {
-	const { name, price, id, count ,onDelete,onIncrement,onDecrement} = props;
+	const { name, price, id, count, onDelete, onIncrement, onDecrement } = props;
 	return (
 		<div className="bg-white text-slate-900 border-2 border-gray-400 p-8 rounded-2xl">
 			<h1 className="text-2xl mb-4">product {name}</h1>
@@ -11,14 +13,24 @@ const Product = (props) => {
 					<span className="w-8 h-8 grid place-content-center bg-purple-300 rounded-full p-1">
 						{count}
 					</span>
-					<button onClick={()=>onIncrement(id)} className="border-2 bg-stone-200 border-gray-400 rounded-md px-2 py-1">
-						increment
+					<button
+						onClick={() => onIncrement(id)}
+						className={`border-2 border-slate-400 bg-stone-200  rounded-md p-2`}>
+						<BiPlus />
 					</button>
-					<button onClick={()=>onDecrement(id)} className="border-2 border-gray-300 rounded-md px-2 py-1">
-						decrement
+					<button
+						onClick={() => onDecrement(id)}
+						className={`border-2 border-gray-300 rounded-md p-2 ${
+							count === 1 ? "text-red-700 border-red-700" : "border-slate-300"
+						}`}>
+						{count === 1 ? <BiTrash /> : <BiMinus />}
 					</button>
 				</div>
-				<button onClick={()=>onDelete(id)} className="px-2 py-1 border-2 rounded-md text-slate-800 self-center">Delete</button>
+				<button
+					onClick={() => onDelete(id)}
+					className="p-2 border-2 rounded-md text-slate-800 self-center">
+					Delete
+				</button>
 			</div>
 		</div>
 	);
