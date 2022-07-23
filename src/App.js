bf50fd2270b43b5fecdfad091238ec04cf03ea2b
alireza0mohammady <mr.afg00@gmail.com>
2022-07-23 21:53:55 +0430

    lifting up products state from ProductList component to App (state was needed in sibling component)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import HeaderNavbar from "./components/HeaderNavbar/HeaderNavbar";
 import ProductsList from "./components/ProductsList/ProductsList";
+import React, { useState } from 'react';
+
 // import HookCounter from "./components/hookcounter/HookCounterClass";
 // import HookCounterFunctional from "./components/hookcounter/HookCounterFunctional";
 // import StateObjectUpdate from "./components/StateObjectUpdate/StateObjectUpdate";
 
 const App = () => {
+
+	const [products, setproducts] = useState([
+		{ name: "react js course", price: 69.99, id: 1, count: 1 },
+		{ name: "node js course", price: 59.99, id: 2, count: 1 },
+		{ name: "next js course", price: 99.99, id: 3, count: 1 },
+	]);
+
+	const deleteProduct = (id) => {
+		const filteredProduct = products.filter((product) => product.id !== id);
+		setproducts(filteredProduct);
+	};
+	const incrementProduct = (id) => {
+		const productsCopy = [...products];
+		const incrementingProduct = productsCopy.find((product) => product.id === id);
+		incrementingProduct.count++;
+		setproducts(productsCopy);
+	};
+	const decrementProduct = (id) => {
+		const productsCopy = [...products];
+		const decrementingProduct = productsCopy.find((product) => product.id === id);
+		if (decrementingProduct.count === 1) {
+			deleteProduct(id);
+			return;
+		}
+		decrementingProduct.count--;
+		setproducts(productsCopy);
+	};
+
 	return (
 		<div className="h-screen bg-stone-50">
-			<HeaderNavbar />
+			<HeaderNavbar products={products}/>
 			<div className="flex flex-col" id="products">
-				<ProductsList />
+				<ProductsList products={products} onDelete={deleteProduct} onIncrement={incrementProduct} onDecrement={decrementProduct} />
 			</div>
 		</div>
 	);
--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,32 +1,9 @@
 import Product from "../Product/Product";
-import React, { useState } from "react";
+
 
 const ProductsList = (props) => {
-	const [products, setproducts] = useState([
-		{ name: "react js course", price: 69.99, id: 1, count: 1 },
-		{ name: "node js course", price: 59.99, id: 2, count: 1 },
-		{ name: "next js course", price: 99.99, id: 3, count: 1 },
-	]);
-	const deleteProduct = (id) => {
-		const filteredProduct = products.filter((product) => product.id !== id);
-		setproducts(filteredProduct);
-	};
-	const incrementProduct = (id) => {
-		const productsCopy = [...products];
-		const incrementingProduct = productsCopy.find((product) => product.id === id);
-		incrementingProduct.count++;
-		setproducts(productsCopy);
-	};
-	const decrementProduct = (id) => {
-		const productsCopy = [...products];
-		const decrementingProduct = productsCopy.find((product) => product.id === id);
-		if (decrementingProduct.count === 1) {
-			deleteProduct(id);
-			return;
-		}
-		decrementingProduct.count--;
-		setproducts(productsCopy);
-	};
+	
+	const {products,onDecrement,onDelete,onIncrement}=props
 	return (
 		<div>
 			{products.length ? (
@@ -35,9 +12,9 @@ const ProductsList = (props) => {
                   products.map((product) => {
                      return (
                         <Product
-                           onIncrement={incrementProduct}
-                           onDecrement={decrementProduct}
-                           onDelete={deleteProduct}
+                           onIncrement={onIncrement}
+                           onDecrement={onDecrement}
+                           onDelete={onDelete}
                            name={product.name}
                            key={product.id}
                            id={product.id}
