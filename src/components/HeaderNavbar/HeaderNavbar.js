f23fb4e24e95d0d623cca6c79f2b1b48ae6a2c93
alireza0mohammady <mr.afg00@gmail.com>
2022-07-23 21:56:54 +0430

    add number of products that is in the cart as a props

--- src/components/HeaderNavbar/HeaderNavbar.js
+++ src/components/HeaderNavbar/HeaderNavbar.js
@@ -1,8 +1,14 @@
 
-const HeaderNavbar = () => {
-   return ( <div className="py-3">
-      <h1 className="text-center p-10 text-3xl font-bold text-slate-900">shopping app</h1>
-   </div> );
-}
- 
+const HeaderNavbar = (props) => {
+	const {products}=props
+	return (
+		<header className="py-3 flex justify-center items-center">
+			<h1 className="p-10 text-3xl font-bold text-slate-900">
+				shopping app
+			</h1>
+			<span className="text-xl font-bold">{products.filter((p)=> p.count>0).length}</span>
+		</header>
+	);
+};
+
 export default HeaderNavbar;
