403797295d7ccd4e7c9eae871dc483454b44ac5f
alireza0mohammady <mr.afg00@gmail.com>
2022-07-27 18:14:42 +0430

    fix mutating products state in handelers(incrementProduct,decrementProduct)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import HeaderNavbar from "./components/HeaderNavbar/HeaderNavbar";
 import ProductsList from "./components/ProductsList/ProductsList";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // import HookCounter from "./components/hookcounter/HookCounterClass";
 // import HookCounterFunctional from "./components/hookcounter/HookCounterFunctional";
 // import StateObjectUpdate from "./components/StateObjectUpdate/StateObjectUpdate";
 
 const App = () => {
-
 	const [products, setproducts] = useState([
 		{ name: "react js course", price: 69.99, id: 1, count: 1 },
 		{ name: "node js course", price: 59.99, id: 2, count: 1 },
@@ -19,27 +18,37 @@ const App = () => {
 		setproducts(filteredProduct);
 	};
 	const incrementProduct = (id) => {
+		const productIndex = products.findIndex((product) => product.id === id);
+		const productCopy = { ...products[productIndex] };
+		productCopy.count++;
 		const productsCopy = [...products];
-		const incrementingProduct = productsCopy.find((product) => product.id === id);
-		incrementingProduct.count++;
+		productsCopy[productIndex] = productCopy;
 		setproducts(productsCopy);
 	};
 	const decrementProduct = (id) => {
 		const productsCopy = [...products];
-		const decrementingProduct = productsCopy.find((product) => product.id === id);
-		if (decrementingProduct.count === 1) {
+		const productIndex = productsCopy.findIndex((product) => product.id === id);
+		if (productsCopy[productIndex].count === 1) {
 			deleteProduct(id);
 			return;
 		}
-		decrementingProduct.count--;
+		const productCopy = { ...products[productIndex] };
+		productCopy.count--;
+		productsCopy[productIndex] = productCopy;
+		console.log(products)
 		setproducts(productsCopy);
 	};
 
 	return (
 		<div className="h-screen bg-stone-50">
-			<HeaderNavbar products={products}/>
+			<HeaderNavbar products={products} />
 			<div className="flex flex-col" id="products">
-				<ProductsList products={products} onDelete={deleteProduct} onIncrement={incrementProduct} onDecrement={decrementProduct} />
+				<ProductsList
+					products={products}
+					onDelete={deleteProduct}
+					onIncrement={incrementProduct}
+					onDecrement={decrementProduct}
+				/>
 			</div>
 		</div>
 	);
